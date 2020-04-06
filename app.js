84818f976edf8a0bae9bf54311ef84659e79031d
Cind0 <mcind0.ori@gmail.com>
2020-04-06 19:00:44 +0200

    base

---
+++ app.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const express = require('express');
+const app = express();
+
+// Include blog routes 
+const blog = require('./routes/blog');
+
+// Connect to DB
+mongoose.connect('mongodb://localhost/lo3go', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(() => console.error('Could not connect to MongoDB...'));
+
+// include build-in middleware - bodyParser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Init blog routes
+app.use('/api/blog-posts', blog);
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ routes/blog.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const router = require('express').Router();
+const Joi = require('joi');
+
+
+
+
+
+
+module.exports = router;
